70e1347c7eecb2b46314231d2050e00be4ed5b76
Emerick95 <38759779+Emerick95@users.noreply.github.com>
2018-05-10 09:29:58 -0500

    v2.1

--- reloj.py
+++ reloj.py
@@ -6,11 +6,9 @@ t = time.localtime()
 h=t[3]
 m= t[4]
 s= t[5]+1
-x="am"
-y="pm"
-c = y
+
 while s<61:
-        print("Time Ecuador: ",h,":",m,":",s,c)
+        print("Time Ecuador: ",h,":",m,":",s)
         s=s+1
         time.sleep(1)
         if s==60:
@@ -19,12 +17,9 @@ while s<61:
             if m==60:
                 m=0
                 h=h+1
-                if h==12:
+                if h==24:
                     h=0
-                    c=x
-                    if c==x and h==13:
-                        h=1
-                        c=y
+                    
                     
 import time
 t=time.localtime()
